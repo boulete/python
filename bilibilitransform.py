2e1a47ea54bb8780fec7e08bc8804102a65636f4
boulete <31558142+boulete@users.noreply.github.com>
2021-08-28 20:54:12 +0800

    Update bilibilitransform.py

--- bilibilitransform.py
+++ bilibilitransform.py
@@ -1,22 +1,30 @@
 from glob import glob
-from json import load
 from ffmpy import FFmpeg
+from json import load
 
-def output(name,v,a):
-    ff = FFmpeg(inputs={v:None,a:None},
-    	outputs={name +'.mp4':'-vcodec copy -acodec copy'})
+# 转换合并函数
+def transform(v,a,o):
+    ff = FFmpeg(inputs={v:None,a:None},outputs={o:'-vcodec copy -acodec copy'})
     print(ff.cmd)
     ff.run()
-path1s = glob(f'./*/*/entry.json')
-path2s = glob(f'./*/*/80/')
+    
+# 解析json文件，获取标题，将其返回
+def get_title(path):
+    f = open(path,'r',encoding='utf8')
+    data = load(f)
+    return data['title']
+    
+# 获取路径集
+paths1 = glob(f'./*/*/entry.json')
+paths2 = glob(f'./*/*/80/')
 
-for i in range(0,len(path1s)):
-    f = open(path1s[i],'r',encoding='utf8')
-    name = load(f)['title']
-    print(name)
-    v = path2s[i] + 'video.m4s'
-    a = path2s[i] + 'audio.m4s'
-    print(v)
-    print(a)
-    output(name,v,a)
-
+# 遍历路径集，执行函数
+for i in range(len(paths1)):
+    # 原始数据视频流路径
+    v = paths2[i] + 'video.m4s'
+    # 原始数据音频流路径
+    a = paths2[i] + 'audio.m4s'
+    # 输出路径（带标题）
+    o = get_title(paths1[i]) + '.mp4'
+    # 执行合并函数
+    transform(v,a,o)
